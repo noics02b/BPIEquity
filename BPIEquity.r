258bdcc75e567ba934820766fc016709ced7fcb4
Winston Sumalia <wasumalia@gmail.com>
2015-08-26 22:19:33 +0800

    ver1.0 --  NAVPS extraction max of 10 days

--- BPIEquity.r
+++ BPIEquity.r
@@ -10,11 +10,15 @@ getdate <-function(x,y){
  
  ##start reading
  downloadfile<-getURL(baseURL)  #download URL since it is under https
- mem<-read.csv(text=downloadfile)  #store to mem
+ mem<-read.csv(text=downloadfile,stringsAsFactors=FALSE,quote="")  #store to mem
  datalines <- grep("NAVpUGridView_Label",mem[,1],value=TRUE)   #search relevant data
  extract <- gsub(".*>(.*)<.*",'\\1',datalines)  #extract values as string
  table <- matrix(extract, ncol=4, byrow=TRUE)  #reformat using matrix
-  return (table)
+ table.df <- data.frame(table, stringsAsFactors = FALSE)  #convert matrix to data frame
+ colnames(table.df) = c("NAVPS","YOY","YTD","DATE")
+ table.df[1:3]<-sapply(table.df[1:3],as.numeric, USE.NAMES=FALSE)  #convert data to numeric
+ table.df[4]<-lapply(table.df[4],function (x) as.Date(x,"%m/%d/%Y")) #convert data to date format
+  return (table.df)
 }
 
 #<span id=NAVpUGridView_Label4_9>08/10/2015</span>"   .*>(.*)<.*
